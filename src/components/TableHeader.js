6c0c29655bdb24351f5682403bbb7c143479486a
Kori <korneliaszabo@kinsta.com>
2020-07-05 10:11:27 +0200

    change back TableHeader

--- src/components/TableHeader.js
+++ src/components/TableHeader.js
@@ -4,14 +4,18 @@ import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 
-export default ({first, second, third, fourth, fifth}) => (
-  <TableHead>
-    <TableRow>
-      <TableCell className="text-uppercase">{first}</TableCell>
-      <TableCell className="text-uppercase">{second}</TableCell>
-      <TableCell className="text-uppercase">{third}</TableCell>
-      <TableCell className="text-uppercase">{fourth}</TableCell>
-      <TableCell className="text-uppercase">{fifth}</TableCell>
-    </TableRow>
-  </TableHead>
-);
+const TableHeader = () => {
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell className="text-uppercase">id</TableCell>
+        <TableCell className="text-uppercase">name</TableCell>
+        <TableCell className="text-uppercase">gender</TableCell>
+        <TableCell className="text-uppercase">status</TableCell>
+        <TableCell className="text-uppercase" />
+      </TableRow>
+    </TableHead>
+  );
+};
+
+export default TableHeader;
